62d269a06fe13b038b16facdbd9a20ff43c9c656
Diego Ghersi <diego@ghersi.com.ar>
2014-12-19 14:21:38 -0300

    - Update django urls

--- web-app/web/urls.py
+++ web-app/web/urls.py
@@ -3,6 +3,11 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from web.views import home_page
 
+# admin
+from django.contrib import admin
+admin.autodiscover()
+
 urlpatterns = patterns('',
+    (r'^admin/', include(admin.site.urls)),
     url(r'^$', home_page),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
