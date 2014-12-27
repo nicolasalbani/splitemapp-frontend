d7290062c1f41152f2a64d4521d7c7a70170b141
Diego Ghersi <diego@ghersi.com.ar>
2014-12-27 13:54:48 -0300

    - Cleanup

--- web-app/web/views.py
+++
@@ -1,8 +0,0 @@
-from django.template.loader import get_template
-from django.template import Context
-from django.http import HttpResponse
-
-def home_page(request):
-    t = get_template('home.html')
-    html = t.render(Context())
-    return HttpResponse(html)
